fc6c4059ba04c79cf042033d612ed2ed94081fc4
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-03-04 21:21:30 +0530

    added auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
 import { Box } from '@chakra-ui/react';
+import { ClerkProvider } from '@clerk/nextjs';
 import React, { ReactNode } from 'react';
 import AppWrappers from './AppWrappers';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body id={'root'}>
-        <AppWrappers>{children}</AppWrappers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body id={'root'}>
+          <AppWrappers>{children}</AppWrappers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
